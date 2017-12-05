73a4f70b9633c12c354800beb236911d3e64455d
xjt <xiongjuntao@imcoming.cn>
2017-12-05 10:05:52 +0800

    Initial Commit

---
+++ 新浪微博swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  新浪微博swift
+//
+//  Created by xjt on 2017/12/5.
+//  Copyright © 2017年 xjt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
